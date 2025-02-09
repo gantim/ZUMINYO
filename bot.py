12ab1d9ca73b210878ba1b3e06c2210dc347ff52
Quenim <KN17022004@yanddex.ru>
2025-02-10 04:58:44 +0500

    Add !ping

--- bot.py
+++ bot.py
@@ -24,6 +24,10 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     print(f"Бот {bot.user} успешно запущен!")
 
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
 async def send_to_discord(message):
     channel = bot.get_channel(DISCORD_CHANNEL_ID)
     if channel:
@@ -44,29 +48,23 @@ async def telegram_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await send_to_discord(message_text)
 
 async def start_telegram_bot():
-    # Создаём приложение Telegram
     application = Application.builder().token(TELEGRAM_TOKEN).build()
 
-    # Добавляем обработчики текстовых сообщений для ЛС, групп и каналов
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, telegram_handler))
     application.add_handler(MessageHandler(filters.UpdateType.CHANNEL_POST, telegram_handler))  # Для каналов
 
-    # Инициализация Telegram-бота
     await application.initialize()
 
-    # Запуск Telegram-бота
     await application.start()
     print("Telegram-бот запущен!")
 
-    # Удерживаем Telegram-бота в работе с очисткой ожидающих запросов
     await application.updater.start_polling(drop_pending_updates=True)
 
 async def main():
-    # Запускаем Telegram-бота и Discord-бота одновременно
+
     telegram_task = asyncio.create_task(start_telegram_bot())
     discord_task = asyncio.create_task(bot.start(DISCORD_TOKEN))
 
-    # Ожидаем завершения обеих задач
     await asyncio.gather(telegram_task, discord_task)
 
 # Запуск основного цикла событий
